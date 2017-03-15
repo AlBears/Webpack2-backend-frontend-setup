0225cdb53d0c6e954afdb1dacaca89d8b534290c
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-15 11:42:47 +0300

    server

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,51 @@
 import 'source-map-support/register';
 
-import { foo } from 'shared/test';
+import express from 'express';
+import http from 'http';
+import socketIo from 'socket.io';
 
-foo();
-console.log('From server with love');
+const isDevelopment = process.env.NODE_ENV !== "production",
+
+//------------------------------
+//Setup
+	app = express(),
+	server = new http.Server(app),
+	io = socketIo(server);
+
+
+
+//------------------------------
+// Client webpack
+
+//----------------------------
+// Configure express
+
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+const useExternalStyles = !isDevelopment;
+app.get('/', (req, res) => {
+	res.render('index', {
+		useExternalStyles
+	});
+});
+
+//------------------------------
+// Modules
+
+//----------------------------
+// Socket
+io.on('connection', socket => {
+	console.log(`Got connection from ${socket.request.connection.remoteAddress}`);
+});
+
+//------------------------------
+// Startup
+const port = process.env.PORT || 3000;
+function startServer() {
+	server.listen(port, () => {
+		console.log(`Starting http server on ${port}`);
+	});
+}
+
+startServer();
